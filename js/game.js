cc3b51624df22ad956f1a6b8ae8f4795067e627d
Jean Abed <jean.abed@hotmail.com>
2020-09-04 13:40:59 +1000

    added FPS counter

--- js/game.js
+++ js/game.js
@@ -1,284 +1,4 @@
-import * as sprites from '/js/sprites.js';
-
-const jumpHeight = 40;
-const damping = 1/150;
-
-class Player {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-
-        this.baseX = x;
-        this.baseY = y;
-
-        this.jumpTime = undefined;
-
-        this.runCount = 0;
-        this.runTime = undefined;
-    }
-
-    get height() {
-        return this.sprite.length;
-    }
-
-    jump() {
-        if (this.jumping === false) {
-            console.log('Jumping');
-
-            this.jumpTime = Date.now();
-        }
-    }
-
-    // Update the x and y values
-    tick() {
-        if (this.jumping) {
-            const newY = this.baseY + this.jumpY;
-
-            if (newY > this.baseY) {
-                this.y = newY;
-            } else {
-                this.y = this.baseY;
-                this.jumpTime = null;
-            }
-        } else {
-            this.y = this.baseY;
-            // buggy i think: this.jumpTime = null; could also do a counter like run
-        }
-    }
-
-    get canRun() {
-        return this.baseX === this.x;
-    }
-
-    get runStep() {
-        if (this.runTime) {
-            const interval = 50;
-            const states = 4;
-
-            const a = (Date.now() - this.runTime);
-
-            if (a > interval) {
-                this.runCount++;
-                this.runCount = this.runCount % states;
-
-                this.runTime = Date.now();
-            }
-        } else {
-            this.runTime = Date.now();
-        }
-
-        return this.runCount;
-    }
-
-    get jumping() {
-        if (this.jumpY > 0) {
-            return true;
-        }
-
-        return false;
-    }
-
-    get jumpY() {
-        if (this.jumpTime) {
-            const a = (Date.now() - this.jumpTime) / 1000;
-
-            const moveBy = -(1/damping) * Math.pow(a - Math.pow(jumpHeight, 1/2) * Math.pow(damping, 1/2), 2) + jumpHeight;
-
-            return Math.floor(moveBy);
-        }
-
-        return 0;
-    }
-
-    get sprite() {
-        if (this.jumping === true) {
-            // Calculate up or down
-            const a = (Date.now() - this.jumpTime) / 1000;
-
-            const half = Math.pow(jumpHeight, 1/2) * Math.pow(damping, 1/2);
-
-            if (a < half) {
-                if (this.jumpY < jumpHeight * 2/3) {
-                    return sprites.upSlime;
-                }
-            } else {
-                if (this.jumpY < jumpHeight * 9/10) {
-                    return sprites.downSlime;
-                }
-            }
-
-            return sprites.normalSlime;
-
-        }
-
-        if (this.canRun) {
-            if (this.runStep === 0) {
-                return sprites.runningSlime;
-            } else if (this.runStep === 1) {
-                return sprites.runningSlime1;
-            } else if (this.runStep === 2) {
-                return sprites.runningSlime2;
-            } else {
-                return sprites.runningSlime3;
-            }
-        }
-
-        return sprites.normalSlime;
-    }
-}
-
-class Ground {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-
-        this.prevTime = undefined;
-
-        this.baseX = x;
-        this.baseY = y;
-
-        this.speed = 75;
-
-        const s = Math.random();
-
-        if (s < 0.6) {
-            this.height = 1;
-        } else if (s < 0.9) {
-            this.height = 2;
-        } else {
-            this.height = 3;
-        }
-    }
-
-    tick() {
-        const now = Date.now();
-
-        if (this.prevTime) {
-            const diff = (now - this.prevTime) / 1000 * this.speed;
-
-            this.x = this.baseX - Math.round(diff);
-        } else {
-            this.prevTime = Date.now();
-        }
-    }
-
-    get sprite() {
-        const sprite = [];
-
-        for (let i = 0; i < this.height; i++) {
-            sprite[i] = [];
-            sprite[i][0] = '|';
-        }
-
-        return sprite;
-    }
-}
-
-class Tree {
-    constructor(x, y) {
-        const s = Math.random();
-
-        if (s < 0.1) {
-            this.height = 19;
-        } else if (s < 0.2) {
-            this.height = 4;
-        } else if (s < 0.4) {
-            this.height = 3;
-        } else if (s < 0.55) {
-            this.height = 6;
-        } else if (s < 0.75){
-            this.height = 7;
-        } else {
-            this.height = 11;
-        }
-
-        this.x = x;
-        this.y = y;
-
-        this.prevTime = undefined;
-
-        this.baseX = this.x;
-        this.baseY = this.y;
-
-        this.speed = 75;
-    }
-
-    tick() {
-        const now = Date.now();
-
-        if (this.prevTime) {
-            const diff = (now - this.prevTime) / 1000 * this.speed;
-
-            this.x = this.baseX - Math.round(diff);
-        } else {
-            this.prevTime = Date.now();
-        }
-    }
-
-    get sprite() {
-        const sprite = [];
-
-        sprite[0] = [];
-        sprite[0][1] = '_';
-        sprite[0][2] = '_';
-        sprite[0][3] = '_';
-
-        for (let i = 1; i < this.height; i++) {
-            sprite[i] = [];
-            sprite[i][1] = '|';
-        }
-
-        return sprite;
-    }
-}
-
-// TODO: speed should not be in here..
-class Platform {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-
-        this.prevTime = undefined;
-
-        this.baseX = x;
-        this.baseY = y;
-
-        this.speed = 75;
-
-        const s1 = Math.random();
-        this.height = Math.round(s1 * 30);
-
-        const s2 = Math.random();
-        this.width = Math.round(s2 * 300);
-    }
-
-    tick() {
-        const now = Date.now();
-
-        if (this.prevTime) {
-            const diff = (now - this.prevTime) / 1000 * this.speed;
-
-            this.x = this.baseX - Math.round(diff);
-        } else {
-            this.prevTime = Date.now();
-        }
-    }
-
-    get sprite() {
-        const sprite = [];
-
-        /*for (let i = 0; i < this.height; i++) {
-            sprite[i] = [];
-            sprite[i][0] = '|';
-        }
-
-        for (let i = 0; i < this.width; i++) {
-            sprite[0][i] = '=';
-        }*/
-
-        return sprite;
-    }
-}
+import * as models from '/js/models.js';
 
 // From: https://stackoverflow.com/a/15666143/3223010
 const PIXEL_RATIO = (function () {
@@ -319,22 +39,22 @@ const lineHeight = fontSize + 1.5;
 const canvas = createHiDPICanvas(canvasWidth, canvasHeight);
 const ctx = canvas.getContext('2d');
 
-ctx.font = `${fontSize}px Menlo`;
-
 const el = document.getElementById('game');
 el.appendChild(canvas);
 
-const approx = canvasWidth / ctx.measureText('.').width;
+//const approx = canvasWidth / ctx.measureText('.').width;
 
-var widthL = Math.floor(approx);
+var widthL = Math.floor(canvasWidth * 412 / 1000);
 var heightL = Math.round(canvasHeight / lineHeight);
 
-const framesPerSecond = 120;
+const framesPerSecond = 60;
+const printDots = false;
 
 const heightFromBottom = 15;
 
-var obstacles = []; // [Obstacles()]
-var player = new Player(15, heightFromBottom);
+const obstacles = []; // [Obstacles()]
+const player = new models.Player(15, heightFromBottom);
+const fpsCounter = new models.FPSCounter(200, 60);
 
 function XYInBounds(x, y) {
     if (y >= 0 && y <= heightL - 1 && x >= 0 && x <= widthL - 1)
@@ -357,6 +77,8 @@ function initMatrix() {
 
 function printMatrix() {
     const t0 = performance.now();
+
+    ctx.font = `${fontSize}px Menlo`;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -382,7 +104,7 @@ function printMatrix() {
                 cache.set(c, textSize);
             }
 
-            if (c === '.') {
+            if (!printDots && c === '.') {
                 x += textSize;
 
                 continue;
@@ -412,17 +134,27 @@ function updateMatrix() {
         var obstacle = obstacles[i];
 
         drawSprite(obstacle.sprite, obstacle.x, obstacle.y);
-
         obstacle.tick();
 
+        // TODO
         if (!XYInBounds(obstacle.x, obstacle.y)) {
             obstacles.splice(i, 1);
         }
     }
 
     drawSprite(player.sprite, player.x, player.y, '#bfe66a');
-
     player.tick();
+}
+
+function printText(text, size, x, y) {
+    ctx.font = `${size}px Menlo`;
+
+    ctx.fillText(text, x, y);
+}
+
+function updateText() {
+    printText(`FPS: ${fpsCounter.fps}`, 10, canvasWidth - 50, 11.5);
+    fpsCounter.tick();
 }
 
 function drawSprite(sprite, x, y, colour) {
@@ -447,19 +179,19 @@ function randomObjectGeneration() {
     var rng = Math.random();
 
     if (rng < 1/(framesPerSecond * 0.1)) {
-        obstacles.push(new Ground(widthL - 1, heightFromBottom - 2));
+        obstacles.push(new models.Ground(widthL - 1, heightFromBottom - 2));
     }
 
     var rng = Math.random();
 
     if (rng < 1/(framesPerSecond * 1)) {
-        obstacles.push(new Tree(widthL - 1, heightFromBottom + 2));
+        obstacles.push(new models.Tree(widthL - 1, heightFromBottom + 2));
     }
 
     var rng = Math.random();
 
     if (rng < 1/(framesPerSecond * 2)) {
-        //obstacles.push(new Platform(widthL - 1, heightL - heightFromBottom));
+        //obstacles.push(new models.Platform(widthL - 1, heightL - heightFromBottom));
     }
 }
 
@@ -481,20 +213,22 @@ document.addEventListener('touchstart', touchstart);
 function l() {
     const now = Date.now();
 
-    if (now - lastUpdate > 1000 / framesPerSecond) {
+    //if (now - lastUpdate > 1000 / framesPerSecond) {
         lastUpdate = now;
 
         randomObjectGeneration();
 
         initMatrix();
-
         updateMatrix();
-
         printMatrix();
-    }
 
-    globalId = requestAnimationFrame(l);
+        updateText();
+    //}
+
+    //globalId = requestAnimationFrame(l);
 }
 
-globalId = requestAnimationFrame(l);
+//globalId = requestAnimationFrame(l);
 // cancelAnimationFrame(globalId);
+
+setInterval(l, Math.round(1000 / framesPerSecond));
---
+++ js/models.js
@@ -0,0 +1,340 @@
+import * as sprites from '/js/sprites.js';
+
+
+class Drawable {
+    drawSprite() {
+        for (let i = this.sprite.length - 1; i >= 0; i--) {
+            const line = this.sprite[i];
+
+            for (let j = line.length - 1; j >= 0; j--) {
+                if (XYInBounds(j + x, heightL - (i + y) - 1) &&
+                    this.sprite[this.sprite.length - i - 1][j] &&
+                    this.sprite[this.sprite.length - i - 1][j] !== '.') {
+
+                    matrix[heightL - (i + y) - 1][x + j] = sprite[sprite.length - i - 1][j];
+
+                    if (colour)
+                        colourMatrix[heightL - (i + y) - 1][x + j] = colour;
+                }
+            }
+        }
+    }
+}
+
+class FPSCounter {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.frameCounter = 0;
+
+        this.prevFps = 0;
+
+        this.prevTime = undefined;
+    }
+
+    tick() {
+        const now = Date.now();
+
+        if (this.prevTime) {
+            const diff = (now - this.prevTime) / 1000;
+
+            if (diff < 1) {
+                this.frameCounter++;
+            } else {
+                this.prevFps = this.frameCounter;
+
+                this.frameCounter = 0;
+                this.prevTime = Date.now();
+            }
+        } else {
+            this.prevTime = Date.now();
+        }
+    }
+
+    get fps() {
+        return this.prevFps;
+    }
+}
+
+const jumpHeight = 40;
+const damping = 1/150;
+
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.baseX = x;
+        this.baseY = y;
+
+        this.jumpTime = undefined;
+
+        this.runCount = 0;
+        this.runTime = undefined;
+    }
+
+    get height() {
+        return this.sprite.length;
+    }
+
+    jump() {
+        if (this.jumping === false) {
+            console.log('Jumping');
+
+            this.jumpTime = Date.now();
+        }
+    }
+
+    // Update the x and y values
+    tick() {
+        if (this.jumping) {
+            const newY = this.baseY + this.jumpY;
+
+            if (newY > this.baseY) {
+                this.y = newY;
+            } else {
+                this.y = this.baseY;
+                this.jumpTime = null;
+            }
+        } else {
+            this.y = this.baseY;
+            // buggy i think: this.jumpTime = null; could also do a counter like run
+        }
+    }
+
+    get canRun() {
+        return this.baseX === this.x;
+    }
+
+    get runStep() {
+        if (this.runTime) {
+            const interval = 50;
+            const states = 4;
+
+            const a = (Date.now() - this.runTime);
+
+            if (a > interval) {
+                this.runCount++;
+                this.runCount = this.runCount % states;
+
+                this.runTime = Date.now();
+            }
+        } else {
+            this.runTime = Date.now();
+        }
+
+        return this.runCount;
+    }
+
+    get jumping() {
+        if (this.jumpY > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    get jumpY() {
+        if (this.jumpTime) {
+            const a = (Date.now() - this.jumpTime) / 1000;
+
+            const moveBy = -(1/damping) * Math.pow(a - Math.pow(jumpHeight, 1/2) * Math.pow(damping, 1/2), 2) + jumpHeight;
+
+            return Math.floor(moveBy);
+        }
+
+        return 0;
+    }
+
+    get sprite() {
+        if (this.jumping === true) {
+            // Calculate up or down
+            const a = (Date.now() - this.jumpTime) / 1000;
+
+            const half = Math.pow(jumpHeight, 1/2) * Math.pow(damping, 1/2);
+
+            if (a < half) {
+                if (this.jumpY < jumpHeight * 2/3) {
+                    return sprites.upSlime;
+                }
+            } else {
+                if (this.jumpY < jumpHeight * 9/10) {
+                    return sprites.downSlime;
+                }
+            }
+
+            return sprites.normalSlime;
+
+        }
+
+        if (this.canRun) {
+            if (this.runStep === 0) {
+                return sprites.runningSlime;
+            } else if (this.runStep === 1) {
+                return sprites.runningSlime1;
+            } else if (this.runStep === 2) {
+                return sprites.runningSlime2;
+            } else {
+                return sprites.runningSlime3;
+            }
+        }
+
+        return sprites.normalSlime;
+    }
+}
+
+class Ground {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.prevTime = undefined;
+
+        this.baseX = x;
+        this.baseY = y;
+
+        this.speed = 75;
+
+        const s = Math.random();
+
+        if (s < 0.6) {
+            this.height = 1;
+        } else if (s < 0.9) {
+            this.height = 2;
+        } else {
+            this.height = 3;
+        }
+    }
+
+    tick() {
+        const now = Date.now();
+
+        if (this.prevTime) {
+            const diff = (now - this.prevTime) / 1000 * this.speed;
+
+            this.x = this.baseX - Math.round(diff);
+        } else {
+            this.prevTime = Date.now();
+        }
+    }
+
+    get sprite() {
+        const sprite = [];
+
+        for (let i = 0; i < this.height; i++) {
+            sprite[i] = [];
+            sprite[i][0] = '|';
+        }
+
+        return sprite;
+    }
+}
+
+class Tree {
+    constructor(x, y) {
+        const s = Math.random();
+
+        if (s < 0.1) {
+            this.height = 19;
+        } else if (s < 0.2) {
+            this.height = 4;
+        } else if (s < 0.4) {
+            this.height = 3;
+        } else if (s < 0.55) {
+            this.height = 6;
+        } else if (s < 0.75){
+            this.height = 7;
+        } else {
+            this.height = 11;
+        }
+
+        this.x = x;
+        this.y = y;
+
+        this.prevTime = undefined;
+
+        this.baseX = this.x;
+        this.baseY = this.y;
+
+        this.speed = 75;
+    }
+
+    tick() {
+        const now = Date.now();
+
+        if (this.prevTime) {
+            const diff = (now - this.prevTime) / 1000 * this.speed;
+
+            this.x = this.baseX - Math.round(diff);
+        } else {
+            this.prevTime = Date.now();
+        }
+    }
+
+    get sprite() {
+        const sprite = [];
+
+        sprite[0] = [];
+        sprite[0][1] = '_';
+        sprite[0][2] = '_';
+        sprite[0][3] = '_';
+
+        for (let i = 1; i < this.height; i++) {
+            sprite[i] = [];
+            sprite[i][1] = '|';
+        }
+
+        return sprite;
+    }
+}
+
+// TODO: speed should not be in here..
+class Platform {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.prevTime = undefined;
+
+        this.baseX = x;
+        this.baseY = y;
+
+        this.speed = 75;
+
+        const s1 = Math.random();
+        this.height = Math.round(s1 * 30);
+
+        const s2 = Math.random();
+        this.width = Math.round(s2 * 300);
+    }
+
+    tick() {
+        const now = Date.now();
+
+        if (this.prevTime) {
+            const diff = (now - this.prevTime) / 1000 * this.speed;
+
+            this.x = this.baseX - Math.round(diff);
+        } else {
+            this.prevTime = Date.now();
+        }
+    }
+
+    get sprite() {
+        const sprite = [];
+
+        /*for (let i = 0; i < this.height; i++) {
+            sprite[i] = [];
+            sprite[i][0] = '|';
+        }
+
+        for (let i = 0; i < this.width; i++) {
+            sprite[0][i] = '=';
+        }*/
+
+        return sprite;
+    }
+}
+
+export { Player, Tree, Ground, Platform, FPSCounter };
